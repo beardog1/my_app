db18f8191f7beaf8de234ea720708bdd3f83ffcb
Beardog <beardog@gmail.com>
2015-12-02 14:46:55 -0700

    adding files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,15 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def create
+      @post = Post.new(post_params)
+      if @post.save
+        redirect_to posts_path
+      else
+        render :new
+      end
+    end
+
   def show
     @post = Post.find(params[:id])
   end
@@ -12,14 +21,18 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  def create
-    @posts = Post.new(post_params)
-    if @post.save
-      redirect_to posts_path
-    else
-      render :new
-    end
+def edit
+  @post = Post.find(params[:id])
+end
+def update
+  @post = Post.find(params[:id])
+  if @post.update(post_params)
+        redirect_to posts_path
+      else
+        render :edit
+      end
   end
+
 
   private
 
